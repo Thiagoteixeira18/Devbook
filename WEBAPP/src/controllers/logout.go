16882ad8b9f0d91d2175f84295d63f75ac736dc6
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-07-22 18:42:19 -0300

    adicionado pacote templates para html, com rodape, cabecalho e scripts

--- WEBAPP/src/controllers/logout.go
+++ WEBAPP/src/controllers/logout.go
@@ -5,7 +5,7 @@ import (
 	"webapp/src/cookies"
 )
 
-//FazerLogout remove os dados de autenticação salva no browser do usuario
+//FazerLogout remove os dados de autenticação salva no browser do usuario.
 func FazerLogout(w http.ResponseWriter, r *http.Request) {
 	cookies.Deletar(w)
 	http.Redirect(w, r, "/login", 302)
