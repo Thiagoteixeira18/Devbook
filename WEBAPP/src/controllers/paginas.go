a80a1003c54ffebf9604611ec6ff2c3df7edef91
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-07-24 11:34:07 -0300

    adicionado ajax para rota de curtidas

--- WEBAPP/src/controllers/paginas.go
+++ WEBAPP/src/controllers/paginas.go
@@ -27,12 +27,12 @@ func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
 
-// CarregarPaginaDeCadastroDeUsuario carrega a pagina de cadastro de usuario.
+// CarregarPaginaDeCadastroDeUsuario carrega a pagina de cadastro de usuario
 func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "cadastro.html", nil)
 }
 
-// CarregarPaginaPrincipal carrega a pagina principal com as publicações.
+// CarregarPaginaPrincipal carrega a pagina principal com as publicações
 func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
 	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
