6d4978417bba10bcfcfdd725f40fc3072f43951a
Thiagoteixeira18 <thiagoteixeira.epo@gmail.com>
2023-07-26 10:39:12 -0300

    adicionado perfil do usuario

--- WEBAPP/src/modelos/publicacao.go
+++ WEBAPP/src/modelos/publicacao.go
@@ -2,7 +2,7 @@ package modelos
 
 import "time"
 
-//Publicacao representa uma publicação feita pelo usuario.
+//Publicacao representa uma publicação feita pelo usuario
 type Publicacao struct {
 	ID        uint64    `json:"id,omitempty"`
 	Titulo    string    `json:"titulo,omitempty"`
